6eab6188482603b1148c6869f8d5fcf19499bcdf
venkat <118790430+Venkatknight2295@users.noreply.github.com>
2024-07-06 23:33:20 +0530

    completed best portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
+import Clients from "../components/Clients";
+import Experiences from "@/components/Experiences";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
   
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[{name: 'Home', link: '/', icon: <FaHome />}]}/>
+        <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experiences />
+        <Approach />
+        <Footer />
       </div>
     </main>
   
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -86,7 +86,7 @@ export const BentoGridItem = ({
        </div>
        {id === 6 && (
         <BackgroundGradientAnimation>
-          <div className="absolute z-50 flex items-center justify-center text-white font-bold"></div>
+         
         </BackgroundGradientAnimation>
        )}
       
