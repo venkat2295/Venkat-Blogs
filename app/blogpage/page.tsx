9efc94debaee6d43e88a42687813b72aa230dcc4
venkat <118790430+Venkatknight2295@users.noreply.github.com>
2024-07-09 14:33:54 +0530

    vercel bugs

--- app/blogpage/page.tsx
+++ app/blogpage/page.tsx
@@ -1,23 +1,19 @@
-
+import React, { Suspense } from 'react';
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-
-
 import Footer from "@/components/Footer";
 import Blogpagecontent from "@/app/Blogpagecontent";
 
-
 export default function Blogpage() {
   return (
-  
-    <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
-      <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems}/>
-        <Blogpagecontent />
-        
-        <Footer />
-      </div>
-    </main>
-  
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
+        <div className="max-w-7xl w-full">
+          <FloatingNav navItems={navItems}/>
+          <Blogpagecontent />
+          <Footer />
+        </div>
+      </main>
+    </Suspense>
   );
 }
