70ed78197959113ad10611ad753d4298b63e225e
venkat <118790430+Venkatknight2295@users.noreply.github.com>
2024-07-09 12:37:49 +0530

    vercel bug changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
         <div className='flex items-center md:gap-3 gap-6'>
            {socialMedia.map((profile)=>(
             <a key={profile.id} href={profile.link} target='_blank' className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black- rounded-lg border border-black-300'>
-                  <img src={profile.img} alt={profile.id} width={20} height={20}/>
+                  <img src={profile.img} alt={String(profile.id)} width={20} height={20}/>
             </a>
            ))}
         </div>
