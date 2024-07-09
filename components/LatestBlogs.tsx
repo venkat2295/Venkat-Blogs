9ea6e47afa5b548685666a98fd9f33ecd8464ffd
venkat <118790430+Venkatknight2295@users.noreply.github.com>
2024-07-09 12:42:29 +0530

    vercel bugs

--- components/LatestBlogs.tsx
+++ components/LatestBlogs.tsx
@@ -10,9 +10,9 @@ const LatestBlogs = () => {
         <span className='text-[#3946d8]'>recent blogs</span>
        </h1>
        <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
-         {projects.map(({id,title,des,img,iconLists,link})=>(
+         {projects.map(({id,title,des,img})=>(
             <div key={id} className=' sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem]  flex items-center justify-center sm:w-[570px] w-[80vw]'>
-               <ThreeDCardDemo id={id} title={title} des={des} img={img} iconLists={iconLists} link={link} />
+               <ThreeDCardDemo id={id} title={title} des={des} img={img}  />
             </div>
 ))}
        </div>
